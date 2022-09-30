13e5265dd0ea7923ffcfca56ff36277a3f75c910
MarielGuZa <mguti122@itam.mx>
2022-09-30 09:55:16 -0500

    Problema 1 y 2 lab

---
+++ LabJava/src/Lab2/PruebaEspecie.java
@@ -0,0 +1,45 @@
+package Lab2;
+
+public class PruebaEspecie {
+
+	public static void main(String[] args) {
+		Especie perry = new Especie("Ornitorrinco", 3000, 0.25);
+		Especie perry2 = new Especie("Ornitorrinco", 3000, 0.25);
+		Especie shamu = new Especie("Orca", 5000, 0.25);
+
+		//Calcular poblacion futura
+		System.out.println("\nPOBLACION FUTURA");
+		System.out.println("En 3 anios van a haber " + perry.calculaPob(3));
+		
+		//Igualdad Especies
+		System.out.println("\nSON IGUALES LAS ESPECIES");
+		System.out.println("Las especies son iguales: "+perry.equals(perry2));
+		System.out.println("Las especies son iguales: "+perry.equals(shamu));
+
+		//Comparar Especies
+		System.out.println("\nMAYOR POBLACION");
+		int pob = perry.compareTo(shamu);
+		if (pob == -1) {
+			System.out.println("La segunda especie es mas grande");
+		} else if (pob == 1)
+			System.out.println("La primera especie es mas grande");
+		else
+			System.out.println("Los dos especies son igual de grandes");
+		
+		pob = perry.compareTo(perry2);
+		if (pob == -1) {
+			System.out.println("La segunda especie es mas grande");
+		} else if (pob == 1)
+			System.out.println("La primera especie es mas grande");
+		else
+			System.out.println("Los dos especies son igual de grandes");
+		
+		
+		//Informacion Especie
+		System.out.println("\nINFO ESPECIE");
+		System.out.println(perry2.toString());
+		
+
+	}
+
+}
---
+++ LabJava/src/Lab2/PruebaJuegoBasketball.java
@@ -0,0 +1,36 @@
+package Lab2;
+
+import java.util.Scanner;
+
+public class PruebaJuegoBasketball {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		//Ej.1
+		String e1, e2;
+		System.out.println("Como se llama el primer equipo?");
+		e1 = input.nextLine();
+		System.out.println("Como se llama el segundo equipo?");
+		e2 = input.nextLine();
+
+		JuegoBasketball partido1 = new JuegoBasketball(e1, e2);
+		
+//		System.out.println(partido1.toString()+"\n");
+//		
+//		//Agrega puntos y checa ganador
+//		System.out.println("Se logró anadir los puntos: " + partido1.anotacion(3, 2));
+//		System.out.println(partido1.toString());
+//		System.out.println(partido1.ganador()+"\n");
+//		
+//		System.out.println("Se logró anadir los puntos: " + partido1.anotacion(2, 1));
+//		System.out.println("Se logró anadir los puntos: " + partido1.anotacion(2, 1));
+//		System.out.println(partido1.toString());
+//		System.out.println(partido1.ganador()+"\n");
+		
+		//Checa marcador de un equipo
+		//sysout ctrl+space
+		System.out.println("El equipo de los Warriors tiene "+ partido1.marcador("Warriors") +" puntos.");
+	}
+
+}
